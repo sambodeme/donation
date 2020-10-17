99fa374adc7f7a02b1a53c1c8d2085b5ae912e9b
AbdoulAzizCOMPAORE <42704262+AbdoulAzizCOMPAORE@users.noreply.github.com>
2020-10-17 20:06:32 +0200

    proposition des methodes pour le controller

---
+++ src/main/java/com/bu/donation/Controller/AppController.java
@@ -0,0 +1,24 @@
+package com.bu.donation.Controller;
+
+public class AppController {
+
+    /*Methodes
+    * methode pour afficher la liste des dons
+    * methode pour afficher la liste des donateurs
+    * methode pour enregistrer un don
+    * methode pour enregistrer un donateur
+    * methode pour supprimer un don
+    * methode pour supprimer un donateur
+    */
+
+
+    /* Services
+    * CategoryService
+    * DonationService
+    * DonorService
+    * PaymentService
+     */
+
+
+
+}
